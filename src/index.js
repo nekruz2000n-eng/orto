d1c3458a08389aab2f3e9ed9ff8caaad5bcdadaf
nekruz2000n-eng <nekruz2000n@gmail.com>
2026-04-19 15:39:33 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,18 +9,18 @@ export default {
       const text = body.message?.text;
       
       if (text === '/start') {
-        const telegramUrl = https://api.telegram.org/bot${token}/sendMessage;
+        const telegramUrl = 'https://api.telegram.org/bot' + token + '/sendMessage';
         await fetch(telegramUrl, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ chat_id: chatId, text: 'Привет! Я бот по ортопедии.' })
         });
       } else if (text) {
-        const telegramUrl = https://api.telegram.org/bot${token}/sendMessage;
+        const telegramUrl = 'https://api.telegram.org/bot' + token + '/sendMessage';
         await fetch(telegramUrl, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ chat_id: chatId, text: Твой вопрос: ${text} })
+          body: JSON.stringify({ chat_id: chatId, text: 'Твой вопрос: ' + text })
         });
       }
       return new Response('OK');
